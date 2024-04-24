fbe928cede04a05b73c0eff1b5af144a5e7673a9
VictorManuelVera <160360761+VictorManuelVera@users.noreply.github.com>
2024-04-24 10:48:06 -0500

    Update ARBOLES VERA.cpp

--- ARBOLES VERA.cpp
+++ ARBOLES VERA.cpp
@@ -1,21 +1,21 @@
 /*
 Realizar un
-sistema que permita el registro de estudiantes con el uso de ·rboles binarios,
-el sistema debe permitir realizar la indexaciÛn (orden de inserciÛn) a travÈs
-del cÛdigo del estudiante (cada estudiante ya tiene un cÛdigo asignado por
-tanto se le debe preguntar) y tambiÈn por la fecha de nacimiento.
+sistema que permita el registro de estudiantes con el uso de √°rboles binarios,
+el sistema debe permitir realizar la indexaci√≥n (orden de inserci√≥n) a trav√©s
+del c√≥digo del estudiante (cada estudiante ya tiene un c√≥digo asignado por
+tanto se le debe preguntar) y tambi√©n por la fecha de nacimiento.
 
 
 
 El sistema debe
-permitir recorrer la soluciÛn planteada con el uso de los mÈtodos de Inorden,
+permitir recorrer la soluci√≥n planteada con el uso de los m√©todos de Inorden,
 Preorden y Postorden.
 
 
 
 Puede darse el
 caso de que dos estudiantes proporcionen la misma fecha de nacimiento en cuyo
-caso deber· ubicarse al nuevo nodo a la derecha de su igual
+caso deber√° ubicarse al nuevo nodo a la derecha de su igual
 
 
 
@@ -25,7 +25,7 @@ permitir eliminar el registro del estudiante.
 
 
 Del alumno se
-deben solicitar: cÛdigo, nombres, apellidos y fecha de nacimiento (aÒo, mes y dÌa)
+deben solicitar: c√≥digo, nombres, apellidos y fecha de nacimiento (a√±o, mes y d√≠a)
 */
 
 #include <iostream>
@@ -42,72 +42,79 @@ struct arbol{
 	int year;
 	int mes;
 	int dia;
+	arbol *izq, *der;
 	
-	*sig;
 };
 
-	arbol *aux, *aux2, *raiz, *izq, *der;
+	arbol *raiz,*raizF,*aux, *aux2;
+/*
+void registro();{
+posicionar();
+}
 
-void registro();
 void mostrar();
 void eliminar();
+*/
 
-int main(){
+//hay un error al implementar esta funcion en registrar
+void posicionar(){
 	
-	int opc=0;
-	
-	do{
-	
-	cout<<"\n\tMENU\n";
-	cout<<"\n\t1. Registrar\n";
-	cout<<"\t2. Mostrar\n";
-	cout<<"\t3. Eliminar\n";
-	cout<<"\t4. Salir\n";
-	cout<<"\nDigite la opcion que desea realizar: ";
-	cin>>opc;
-	
-		switch (opc){
-		
-		case 1:
-			
-			registro();
-			break;
-				
-		case 2:
-			
-			mostrar();
-			break;
-			
-		case 3:
-			
-			eliminar();
-			break;
-			
-		case 4:
-		
-			cout<<"\nGracias por usar nuestro programa... Saliendo\n";	
-			cout<<"...finalized...";
-			break;
-		
-		default:
-			
-		cout<<"\nLa opcion que digitaste no se encuentra disponible, por favor digite la opcion nuevamente: \n";
-		break;
-		
+	if(aux->codigo<aux2->codigo){
+		if(aux2->izq!=NULL){
+		aux2=aux2->izq;
+		posicionar();	
+		}
+		else{
+			aux2->izq = aux;
+		}
+	}else if(aux->codigo>aux2->codigo){
+		if(aux2->der!=NULL){
+		aux2=aux2->der;
+		posicionar();
 	}
-		
-	}while(opc!=4);
-
-	getch();
-	return 0;
-
-	}
-
+	else{
+			aux2->der = aux;
+		}
+}
 
 void registrar(){
 
-
+aux = (struct arbol *)malloc(sizeof(struct arbol)); 
 	
+cout<<"\nDigite el codigo del estudiante: ";
+cin>>aux->codigo;
+	
+cout<<"\nDigite el nombre del estudiante: ";
+cin>>aux->nombre;
+	
+cout<<"\nDigite el apellido del estudiante: ";
+cin>>aux->apellido;
+	
+cout<<"\nDigite la fecha de nacimiento del estudiante: \n";
+
+cout<<"\nDigite el dia en el que nacio el estudiante: ";
+cin>>aux->dia;
+
+cout<<"\nDigite el mes en el que nacio el estudiante: ";
+cin>>aux->mes;
+
+cout<<"\nDigite el year en el que nacio el estudiante: ";
+cin>>aux->year;
+
+aux->izq = aux->der = NULL;
+
+if(raiz==NULL){
+	
+	raiz = aux;
+	aux = NULL;
+	free(aux);
+}
+
+else if(raiz!=NULL){
+	aux2=raiz;
+	posicionar();
+}
+
 }
 
 void mostrar(){
@@ -122,26 +129,48 @@ void eliminar(){
 	
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int main(){
+	
+	
+	int opc=0;
+	
+	
+	do{
+	
+	cout<<"\n\tMENU\n";
+	cout<<"\n\t1. Registrar\n";
+	cout<<"\t2. Mostrar\n";
+	cout<<"\t3. Eliminar\n";
+	cout<<"\t4. Salir\n";
+	cout<<"\nDigite la opcion que desea realizar: ";
+	cin>>opc;
+		
+	switch(opc){
+		
+		case 1:
+			registrar();
+			break;
+			
+		case 2:
+			mostrar();
+			break;
+			
+		case 3:
+			eliminar();
+			break;
+			
+		case 4:
+			cout<<"\nGracias por usar nuestro programa... Saliendo\n";	
+				cout<<"...finalized...";
+			break;
+			
+		default:
+			cout<<"\nLa opcion que digitaste no se encuentra disponible, por favor digite la opcion nuevamente: \n";
+			break;
+	}
+		
+	}while(opc!=4);
+	
+	getch();
+	return 0;
+}
